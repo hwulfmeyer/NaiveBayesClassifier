f2cb846a54b33f0ccdbdb9a23ca52f0048b89805
wulfihm <hans-martin.wulfmeyer@st.ovgu.de>
2018-01-05 16:04:58 +0100

    added calculation of probabilities of classes and attributes and their values

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import filehandling as fiha
 import naivebayes as naba
 
 
-filepathnames = "datasets/cardaten/carnames.txt"
+filepathnames = "datasets/cardaten/carnames.data"
 filepathdata = "datasets/cardaten/car.data"
+
+filepathnames = "datasets/tennisdaten/tennisnames.data"
+filepathdata = "datasets/tennisdaten/tennis.data"
 
 # read data
 classes, attributes, attribute_values = fiha.read_data_names(filepath=filepathnames)
@@ -14,5 +17,11 @@ instances = fiha.read_data(filepath=filepathdata)
 print("Number of Classes: " + str(len(classes)))
 print("Number of Attributes: " + str(len(attributes)))
 print("Number of Instances: " + str(len(instances)))
+
+class_prob, attrib_prob = naba.calculate_probabilities(classes, attributes, attribute_values, instances)
+
+print(class_prob)
+for attribut in attrib_prob:
+    print(attribut)
 
 
--- naivebayes.py
+++ naivebayes.py
@@ -1,3 +1,51 @@
 """
 This file is for the methods concerning everything naive bayes
+
+1. separate it in 2 clusters(1 Training , 2 Test)
+2. calculate all probabilities from training data [DONE]
+3. afterwards make a function to use this probabilities and to decide to which class it is belonged
 """
+
+
+def calculate_probabilities(classes: list, attributes: list, attribute_values: list, instances: list):
+    """
+
+    :param classes: is a one-dimensional list containing the class names
+    :param attributes: is one dimensional list that contains the names of attributes
+    :param attribute_values: is a two-dimensional list where each row respresents
+            one attribute(in the order of 'attributes') and the possible values
+    :param instances: is a two-dimensional list where each row respresents
+          one attribute(in the order of 'attributes') and the possible values
+    :return:
+    numclassinstances: a 2-dimensional list containg the classes, the total frequency, the number of total instances and the
+    fraction of instances being that class
+    attributeline:  a 4-dimensional list containg the attributes with their values and how many of these values correespond
+    to a specific class
+    """
+    if len(instances) == 0:
+        return 0, []
+    classinstances = [row[-1] for row in instances]
+    numclassinstances = []
+    num_instances = len(classinstances)
+    for dclass in classes:
+        classfrequency = sum(inst[0] == dclass[0] for inst in classinstances)
+        classprobline = [dclass, classfrequency, num_instances, classfrequency/num_instances]
+        numclassinstances.append(classprobline)
+
+    attributeline = []
+    for i in range(0, len(attributes)):
+        valueslines = []
+        for k in range(0, len(attribute_values[i])):
+            classprob = []
+            for p in range(0, len(classes)):
+                classattrbfrequency = sum((inst[-1] == classes[p] and inst[i] == attribute_values[i][k]) for inst in instances)
+                classprob.append([classes[p], classattrbfrequency/numclassinstances[p][1] ])
+
+            valueline = [attribute_values[i][k], classprob]
+            valueslines.append(valueline)
+
+        attributeline.append([attributes[i], valueslines])
+
+    return numclassinstances, attributeline
+
+
